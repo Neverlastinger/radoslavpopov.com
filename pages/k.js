85cb2355b51a8b357f49c255cfa9f5a825e7dc5e
Neverlastinger <shadow_87@mail.bg>
2021-02-28 13:17:28 +0200

    Adds Kredy project mobile version

--- pages/k.js
+++ pages/k.js
@@ -1,22 +1,36 @@
 import Link from 'next/link'
+import { useState } from 'react';
 import Layout from '../components/Layout';
+import { useResize } from '../hooks';
 import styles from './styles.module.scss';
 
-const KredyPage = () => (
-  <Layout>
-    <div className={styles.projectPage}>
-      <div className={styles.projectCard}>
-        <div className={styles.infoCard}>
-          Currently designing and implementing the Front-End architecture of this upcoming start-up idea.
-        </div>
-        <div className={styles.imageWrapper}>
-          <Link href="/">
-            <img src="/images/k-work-in-progress.png" alt="/projects/k" className={styles.projectImage} />
-          </Link>
+export const isMobileWidth = () => (
+  window.matchMedia('(max-width: 626px)').matches
+);
+
+const KredyPage = () => {
+  const [img, setImg] = useState();
+
+  useResize(() => {
+    setImg(isMobileWidth() ? 'k-work-in-progress-mobile.png' : 'k-work-in-progress.png');
+  });
+
+  return (
+    <Layout>
+      <div className={styles.projectPage}>
+        <div className={styles.projectCard}>
+          <div className={styles.infoCard}>
+            Currently designing and implementing the Front-End architecture of this upcoming start-up idea.
+          </div>
+          <div className={styles.imageWrapper}>
+            <Link href="/">
+              <img src={`images/${img}`} alt="/projects/k" className={styles.projectImage} />
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  </Layout>
-);
+    </Layout>
+  );
+};
 
 export default KredyPage;
