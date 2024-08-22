508c74940db3680a59cea37d3f04a3ec5595939f
Radoslav Popov <neverlastinger@gmail.com>
2024-08-22 15:18:59 +0100

    Update project

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-import Link from 'next/link';
-import { SocialIcon } from 'react-social-icons';
-import Layout from '../components/Layout';
-import styles from './index.module.scss';
+import Link from "next/link";
+import { SocialIcon } from "react-social-icons";
+import Layout from "../components/Layout";
+import styles from "./index.module.scss";
 
 const Home = () => (
   <Layout>
     <div className={styles.wrapper}>
-
       <div className={styles.card}>
         <div className={styles.profileImage}>
           <img src="/images/me.png" alt="Radoslav Popov" />
@@ -21,35 +20,42 @@ const Home = () => (
         </ul>
 
         <div className={styles.content}>
-          Bachelor of Informatics, Master of General Psychology,
-          14 years of software engineering experience working in different IT corporations (2007 - 2021),
-          building large-scale applications that last,
-          fond of UX and great products, backpacked S.E. Asia for 4 months at one point in life.
+          Bachelor of Informatics, Master of General Psychology, 14 years of
+          software engineering experience working in different IT corporations
+          (2007 - 2021), building large-scale applications that last, fond of UX
+          and great products, backpacked S.E. Asia for 4 months at one point in
+          life.
         </div>
 
         <ul className={styles.socialIcons}>
           <li>
-            <SocialIcon url="https://www.linkedin.com/in/radoslav-popov-8092aa3b/" target="_blank" />
+            <SocialIcon
+              url="https://www.linkedin.com/in/radoslav-popov-8092aa3b/"
+              target="_blank"
+            />
           </li>
           <li>
-            <SocialIcon url="https://medium.com/@radoslav.popov" target="_blank" />
+            <SocialIcon
+              url="https://medium.com/@radoslav.popov"
+              target="_blank"
+            />
           </li>
         </ul>
       </div>
 
       <div className={styles.footer}>
         <Link href="/i">
-          <a href="/i">
-            <img src="/images/investia.png" alt="View Product" />
-          </a>
+          <img src="/images/investia.png" alt="View Product" />
         </Link>
         <Link href="/k">
-          <a href="/k">
-            <img src="/images/kredy.svg" alt="View Product" />
-          </a>
+          <img src="/images/kredy.svg" alt="View Product" />
         </Link>
-        <a href="https://neverlastinger.github.io/2048-react-redux/build/" target="_blank">
-          <img src="/images/2048.png" alt="View 2048" className={styles.game2048} />
+        <a>
+          <img
+            src="/images/2048.png"
+            alt="View 2048"
+            className={styles.game2048}
+          />
         </a>
       </div>
     </div>
